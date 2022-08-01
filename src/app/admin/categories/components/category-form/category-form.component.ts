a224a6e83628f3d4e84c1a407f072c79e5c249c5
AlexGallardo <alex.ibaceta@gmail.com>
2022-07-31 23:55:12 -0400

    creacion formulario para crear categorias

--- src/app/admin/categories/components/category-form/category-form.component.ts
+++ src/app/admin/categories/components/category-form/category-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-category-form',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  constructor( private formBuiler:FormBuilder) { 
+    this.buildForm();
+  }
 
   ngOnInit(): void {
   }
+form: FormGroup;
+
+private buildForm(){
+  this.form=this.formBuiler.group({
+  name:['',[Validators.required]],
+  image:['',[Validators.required]]
+  })
+}
+
+get nameField(){
+  return this.form.get('name');
+}
+
+get imageField(){
+  return this.form.get('image');
+}
 
 }
